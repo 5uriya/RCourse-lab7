c1840ab559595a5fba8d4fccaf289d03243541f6
samanzahid <samanzahid22@gmail.com>
2017-10-09 21:02:51 +0200

    export class

---
+++ R/ridgereg.R
@@ -0,0 +1,28 @@
+#'A RC class for ridgereg regression
+#'
+#' @field formula The formula object containing depedent and independent variables
+#' @field data A data frame object to apply the multiple linear regression to
+#' @field lambda numeric value which is default set as Zer0
+#' a<-ridgereg$new(formula=Petal.Length ~ Sepal.Width + Sepal.Length,data=iris)
+#' plot(ridgereg$new(formula=Petal.Length ~ Sepal.Width + Sepal.Length,data=iris))
+#' @name ridgereg
+#'@exportClass ridgereg
+#'@export ridgereg
+
+ridgereg <- setRefClass("ridgereg", 
+                        fields = list(formula="formula",data="data.frame",lambda="numeric"),
+                        methods = list(
+                          initialize= function(formula,data,lambda=0)
+                          {
+                            #This function acts as constructor
+                            
+                            formula<<-formula
+                            data<<-data
+                            lambda<<-lambda
+                            
+                           
+                             
+                            
+                          }
+                         )
+                        ) 
