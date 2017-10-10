e25c023a4eae3bddde53fcf907fc7cc668c0bc3e
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-10 03:14:40 +0200

    visual flight implemented

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -1,7 +1,15 @@
+#'@title Visualize ariport Flight delays
+#'@description In this you can see that which flight is delay at what is its mean by its lat lng
+#' 
+#'@examples
+#' visualize_airport_delays()
+#' @export 
+
 visualize_airport_delays <- function()
 {
-  require("tidyverse")
-  require("nycflights13")
+  requireNamespace("tidyverse")
+  require(tidyr)
+  require(nycflights13)
   library(ggplot2)
   
   data(flights)
@@ -20,12 +28,13 @@ visualize_airport_delays <- function()
   flight_delay_mean <- group_data %>% dplyr::summarise('arr_delay_mean' = mean( arr_delay, na.rm = TRUE)) 
   
   #get lat and lng 
-  lat_lng <- group_data %>% dplyr::summarise("cord" = sprintf("lat= %s lon = %s" , lat[1], lon[1]))
+  lat_lng <- group_data %>% dplyr::summarise("cord" = sprintf("lat= %s lon = %s" , lat[1], lon[1]), 'faa' = faa)
 
   
   data_frame <- data.frame(flight_delay_mean,lat_lng[,2])
-  ggplot(data_frame, aes(x = dest, y = arr_delay_mean, label = cord)) + 
+  p<- ggplot(data_frame, aes(x = faa, y = arr_delay_mean, label = cord)) + 
     geom_point() 
+  return(p)
   # ggplot(data_frame,aes(y=mean,x=dest, labels="cordinates"))+geom_point(shape=1,size=3)+xlab(paste("Fitted values", phras, sep = "\n"))+ ylab("Residuals")+ggtitle("Residuals vs Fitted") +geom_hline(yintercept=0, linetype="dashed")+geom_smooth(span = 1.5,colour="red",method="loess",se=FALSE)+annotation_custom(img, xmin = 1.4, xmax = 2.4, ymin = 0.8, ymax = 1.4)+theme_liu()
 }
 
