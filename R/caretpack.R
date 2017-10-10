bb33247ed7bcdeaf2d0e3bf7eececf79d959bc1a
samanzahid <samanzahid22@gmail.com>
2017-10-10 21:09:30 +0200

    caretpack editted

--- R/caretpack.R
+++ R/caretpack.R
@@ -2,24 +2,63 @@ library(caret)
 library(mlbench)
 
 data("BostonHousing")
-Boston_data<-BostonHousing
+boston_data<-BostonHousing
 print(data)
 
 dim(data)
 
-indexes = sample(1:nrow(Boston_data),size = 0.4*nrow(Boston_data)) #randomly separating 40% of data by index
+indexes = sample(1:nrow(boston_data),size = 0.4*nrow(boston_data)) #randomly separating 40% of data by index
 
-test_set<- Boston_data[indexes,] #assigninng 40% data to test
-training<- Boston_data[-indexes,]  #assigning remaining 60% data to training set
+# #indexes = createDataPartition(boston_data$rm, p = .4, 
+#                     list = FALSE, 
+#                     times = 1)
+
+
+test_set<- boston_data[indexes,] #assigninng 40% data to test
+training<- boston_data[-indexes,]  #assigning remaining 60% data to training set
 
 set.seed(12345)
-Ridgereg_Fit <- train(Boston_data$rm ~ ., data = Boston_data, 
-                 method = "lm",
+
+#
+
+ridgereg_fit <- train(rm ~ ., data = boston_data, 
+                 method = "lm"
                  )
-RidgeregFit
+
+lm <- summary(ridgereg_fit)
+lm$coefficients
+
+
+ridgereg_forward_fit <- train(rm ~ ., data = boston_data, 
+                      method = "leapForward")
+
+lm_forward <- summary(ridgereg_forward_fit)
+lm_forward$which
+
 
 # fitControl <- trainControl(## 10-fold CV
 #   method = "repeatedcv",
 #   number = 10,
 #   ## repeated ten times
 #   repeats = 10)
+
+
+#fitting ridge regression model
+
+Ridge <- list(type = "Regression",  #to load package if needed
+              library = "statPack",
+              loop = NULL) 
+
+prm <- data.frame(parameter = "lambda",
+                  class = "numeric",
+                  label = "lambda")
+
+Ridge$parameters <- prm
+
+ridgeFit <- function(x, y, param) { 
+  data <- as.data.frame(x)
+  
+}
+
+
+
