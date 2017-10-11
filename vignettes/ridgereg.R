85171c8b93aa634f594e1d3484af8c40dde459ce
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-11 16:13:29 +0200

    change library to require

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -1,20 +1,17 @@
 ## ------------------------------------------------------------------------
-library(statPack)
-require(caret)
-library(caret)
 library(mlbench)
 data("BostonHousing") #load a data 
-boston_data<-BostonHousing #set a data to variable
-indexes = createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
+boston_data <- BostonHousing #set a data to variable
+indexes = caret::createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
 training<- boston_data[indexes,] #assigninng 70% data to test
 testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
 
 set.seed(12345)
-ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
+ridgereg_fit <- caret::train(rm ~ . , data = training, method = "lm")
 # summary(ridgereg_fit)
 print(ridgereg_fit)
 set.seed(12345)
-ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
+ridgereg_forward_fit <- caret::train(rm ~ ., data = training, method = "leapForward")
 print(ridgereg_forward_fit)
 
 ## ------------------------------------------------------------------------
