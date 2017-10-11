dd09fb7bca5d8cd69582cbd09cebf3a537cf1355
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-11 12:41:51 +0200

    update vignette with resoultion

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -8,13 +8,11 @@ training<- boston_data[indexes,] #assigninng 70% data to test
 testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
 
 ## ------------------------------------------------------------------------
-library(caret) 
-library(mlbench)
 set.seed(12345)
-ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
+ridgereg_fit <- caret::train(rm ~ . , data = training, method = "lm")
 # summary(ridgereg_fit)
 print(ridgereg_fit)
 set.seed(12345)
-ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
+ridgereg_forward_fit <-caret::train(rm ~ ., data = training, method = "leapForward")
 print(ridgereg_forward_fit)
 
