b9145b3df8c426e9b11f4e94bb7cf5d53fa08715
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-11 15:57:05 +0200

    merge master

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -11,11 +11,33 @@ visualize_airport_delays <- function()
   flights <- na.omit(flights)
   airports <- nycflights13::airports
   
-
+  
   #group_by data by dest
   library(dplyr)
-  combine_data <- inner_join(flights, airports, by = c("dest" =  "faa"))
-  flights <- summarise(group_by(flights, dest), M = mean(arr_delay))
+  library(ggplot2)
+  
+  #qyery to combine data
+  # combine_data <- inner_join(airports, flights, by = c ("faa"= "dest"))
+  # group_data <- combine_data %>%  group_by(dest)
+  # flight_delay_mean <- group_data %>% summarise('arr_delay_mean' = mean( arr_delay))
+  # 
+  
+  mean_data <- summarise(group_by(flights, dest), M = mean(arr_delay))
+  combine_data <- inner_join(airports,mean_data, by = c("faa" =  "dest"))
+  
+    ggplot2::ggplot(combine_data, ggplot2::aes(x=combine_data$lat, y=combine_data$lon)) +
+    ggplot2::geom_point(ggplot2::aes(color=combine_data$M), size=3) +
+    ggplot2::scale_color_gradient(low="black", high="#F5F5F5") +
+    ggplot2::theme_bw() +
+    ggplot2::labs(title="Average arrival delays",
+                  subtitle="Longitude vs. Latitude",
+                  x="Latitude", y="Longitude",
+                  color="Arrival delays") +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust=0.5, size=16),
+                   plot.subtitle = ggplot2::element_text(hjust = 0.5, size=14, face="italic"),
+                   axis.text = ggplot2::element_text(size=12))
+  
+  #ggplot(mean_delay, aes(combine_data$lat,combine_data$lon, fill = weight)) + geom_raster() + coord_fixed(ratio = 1)  + scale_fill_gradientn(colours = rev(rainbow(7))
   
   # data_frame <- data.frame(flight_delay_mean,lat_lng[,2])
   # library(ggplot2)
@@ -25,5 +47,5 @@ visualize_airport_delays <- function()
   
 }
 
-# visualize_airport_delays()
+visualize_airport_delays()
 
