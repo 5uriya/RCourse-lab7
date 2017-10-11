aee230ba499435a6e22387522348ec407dd18029
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-11 12:07:16 +0200

    vignette created

---
+++ vignettes/ridgereg.R
@@ -0,0 +1,18 @@
+## ------------------------------------------------------------------------
+library(caret) 
+library(mlbench)
+data("BostonHousing") #load a data 
+boston_data<-BostonHousing #set a data to variable
+indexes = createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
+training<- boston_data[indexes,] #assigninng 70% data to test
+testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
+
+## ------------------------------------------------------------------------
+set.seed(12345)
+ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
+# summary(ridgereg_fit)
+print(ridgereg_fit)
+set.seed(12345)
+ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
+print(ridgereg_forward_fit)
+
