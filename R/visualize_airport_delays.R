e9d09e4c0cef3219f906afd783d75a1e6807fd6f
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-12 02:44:43 +0200

    grapg updated

--- R/visualize_airport_delays.R
+++ R/visualize_airport_delays.R
@@ -8,25 +8,26 @@ visualize_airport_delays <- function()
 {
 
   flights <- nycflights13::flights
-  flights <- na.omit(flights)
+  flights <- stats::na.omit(flights)
   airports <- nycflights13::airports
 
 
   #group_by data by dest
-  library(dplyr)
-  library(ggplot2)
+  # library(dplyr)
+  # library(ggplot2)
 
 
   #calculating mean of flight delays
-  mean_data <- summarise(group_by(flights, dest), M = mean(arr_delay))
+  mean_data <- dplyr::summarise(dplyr::group_by(flights, dest), M = mean(arr_delay))
 
   #qyery to combine data
-  combine_data <- inner_join(airports,mean_data, by = c("faa" =  "dest"))
+  combine_data <- dplyr::inner_join(airports,mean_data, by = c("faa" =  "dest"))
 
-  ggplot(combine_data, aes(x=combine_data$lat, y=combine_data$lon)) +
+  p <- ggplot2::ggplot(combine_data, aes(x=combine_data$lat, y=combine_data$lon)) +
     geom_point(na.rm = TRUE) + theme_gray() +
     labs(title="Average Flight Delays",subtitle="Longitude vs. Latitude",
          x="Latitude", y="Longitude")+theme(plot.title = element_text(hjust = 0.5),plot.subtitle = element_text(hjust = 0.5))
+  return(p)
 }
 
 visualize_airport_delays()
