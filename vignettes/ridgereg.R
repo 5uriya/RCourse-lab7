f261e12bb73e74cc73725ac8c2f4fa6f61c39b61
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-12 04:04:18 +0200

    hold vignette

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -1,24 +1,24 @@
 ## ---- echo=FALSE , results= "hide"---------------------------------------
-library(statPack)
-library(caret)
-library(lattice)
-require(ggplot2)
-library(mlbench)
+# library(statPack)
+# library(caret)
+# library(lattice)
+# require(ggplot2)
+# library(mlbench)
 
 ## ------------------------------------------------------------------------
 
-data("BostonHousing") #load a data 
-boston_data <- BostonHousing #set a data to variable
-indexes = caret::createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
-training<- boston_data[indexes,] #assigninng 70% data to test
-testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
-
-set.seed(12345)
-ridgereg_fit <- caret::train(rm ~ . , data = training, method = "lm")
-# summary(ridgereg_fit)
-print(ridgereg_fit)
-ridgereg_forward_fit <- caret::train(rm ~ ., data = training, method = "leapForward")
-print(ridgereg_forward_fit)
+# data("BostonHousing") #load a data 
+# boston_data <- BostonHousing #set a data to variable
+# indexes = caret::createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
+# training<- boston_data[indexes,] #assigninng 70% data to test
+# testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
+# 
+# set.seed(12345)
+# ridgereg_fit <- caret::train(rm ~ . , data = training, method = "lm")
+# # summary(ridgereg_fit)
+# print(ridgereg_fit)
+# ridgereg_forward_fit <- caret::train(rm ~ ., data = training, method = "leapForward")
+# print(ridgereg_forward_fit)
 
 ## ------------------------------------------------------------------------
 # set.seed(12345)
