d6985151f57d4667e23a5d3c7e552d0feaf8e5e1
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-12 14:13:17 +0200

    train line ucommented

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -10,9 +10,10 @@ indexes = createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
 training<- boston_data[indexes,] #assigninng 70% data to test
 testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
 
-# set.seed(12345)
-# ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
-# # summary(ridgereg_fit)
+## ------------------------------------------------------------------------
+set.seed(12345)
+ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
+summary(ridgereg_fit)
 # print(ridgereg_fit)
 # ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
 # print(ridgereg_forward_fit)
