48989e0f543715ec2fcfe670d90ebca10b8dfab9
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-12 13:46:05 +0200

    Rmd file code uncommented

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -5,17 +5,17 @@ library(statPack)
 
 ## ------------------------------------------------------------------------
 data("BostonHousing") #load a data
-boston_data <- BostonHousing #set a data to variable
-indexes = createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
-training<- boston_data[indexes,] #assigninng 70% data to test
-testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
+# boston_data <- BostonHousing #set a data to variable
+# indexes = createDataPartition(boston_data$rm, p = .70, list = FALSE, times = 1)
+# training<- boston_data[indexes,] #assigninng 70% data to test
+# testing<- boston_data[-indexes,]  #assigning remaining 30% data to training set
 
-set.seed(12345)
-ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
-# summary(ridgereg_fit)
-print(ridgereg_fit)
-ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
-print(ridgereg_forward_fit)
+# set.seed(12345)
+# ridgereg_fit <- train(rm ~ . , data = training, method = "lm")
+# # summary(ridgereg_fit)
+# print(ridgereg_fit)
+# ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
+# print(ridgereg_forward_fit)
 
 ## ------------------------------------------------------------------------
 # set.seed(12345)
