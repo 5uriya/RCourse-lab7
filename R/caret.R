fb65da363189922b3de9d20434c62e1fecd03ab6
samanzahid <samanzahid22@gmail.com>
2017-10-13 04:24:59 +0200

    predictors and response var changed to formula in caret

--- R/caret.R
+++ R/caret.R
@@ -1,4 +1,4 @@
-#library(statPack)
+library(statPack)
 library(caret)
 # library(lattice)
 #require(ggplot2)
@@ -26,66 +26,91 @@ print(ridgereg_forward_fit)
 #   repeats = 10)
 
 
-#fitting ridge regression model
 
-# RidgeModel <- list(type = "Regression",library = "statPack",loop = NULL)  #to load package if needed
-# 
-# prm <- data.frame(parameter = "lambda",class = "numeric",label = "lambda")
-# 
-# RidgeModel$parameters <- prm
-# 
-# print(RideModel)
 
 names(getModelInfo())
 
 fitControl<-trainControl(method = "repeatedcv",repeats = 8)
 
+
 RidgeModel <- list(label = "Ridge Regression",
-                  library = "statPack",
-                  type = "Regression",
-                  parameters = data.frame(parameter = c('lambda'),
-                                          class = c("numeric"),
-                                          label = c('Weight Decay')),
-                  grid = function(x, y, len = NULL, search = "grid")  {
-                    if(search == "grid") {
-                      out <- expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)))
-                    } else {
-                      out <- data.frame(lambda = 10^runif(len, min = -5, 1))
-                    }
-                    out
-                  },
-                  loop = NULL,
-                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
-                    statPack::ridgereg(as.matrix(x), y, lambda = param$lambda)
-                  },
-                  predict = function(modelFit, newdata, submodels = NULL) {
-                    predict(modelFit, newdata, 
-                            s = 1, 
-                            mode = "fraction")$fit
-                  },
-                  predictors = function(x, s = NULL, ...) {
-                    if(is.null(s))
-                    {
-                      if(!is.null(x$tuneValue))
-                      {
-                        s <- x$tuneValue$.fraction
-                      } else stop("must supply a vaue of s")
-                      out <- predict(x, s = s,
-                                     type = "coefficients",
-                                     mode = "fraction")$coefficients
-                      
-                    } else {
-                      out <- predict(x, s = s)$coefficients
-                      
-                    }
-                    names(out)[out != 0]
-                  },
-                  tags = c("Linear Regression", "L2 Regularization"),
-                  prob = NULL,
-                  sort = function(x) x[order(-x$lambda),])
+              library = "statPack",
+              type = "Regression",
+     parameters = data.frame(parameter = c('lambda'),
+                             class = c("numeric"),
+                             label = c('Weight Decay')),
+     grid = function(x, y, len = NULL, search = "grid")  {
+       if(search == "grid") {
+         out <- expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)))
+       } else {
+         out <- data.frame(lambda = 10^runif(len, min = -5, 1))
+       }
+       out
+     },
+     loop = NULL,
+     fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+       
+       #creating df of predictors
+       dat <- as.data.frame(x)
+       
+       respvector <- NULL
+       respname <- NULL
+       respnum <- NULL
+       
+       
+       for(i in 1:ncol(x)){
+         if(identical(y,dat[,i])){
+           respvector <- dat[,i]
+           respname <- names(x)[i]
+           respnum <- i
+         }
+       }
+       
+       formula <- paste(respname,"~", sep="")
+       
+       if(ncol(x) > 1){
+         for(i in 1:ncol(x)){
+           if(i != respnum){
+             formula <- paste(formula, "+", names(dat)[i], sep="")
+           }
+         }
+       }
+       
+       formula <- as.formula(formula)
+       print(formula)
+      
+      r<-statPack::ridgereg(formula, dat, lambda = param$lambda)
+      
+      },
+     predict = function(modelFit, newdata, submodels = NULL) {
+       predict(modelFit, newdata, 
+               s = 1, 
+               mode = "fraction")$fit
+     },
+     predictors = function(x, s = NULL, ...) {
+       if(is.null(s))
+       {
+         if(!is.null(x$tuneValue))
+         {
+           s <- x$tuneValue$.fraction
+         } else stop("must supply a vaue of s")
+         out <- predict(x, s = s,
+                        type = "coefficients",
+                        mode = "fraction")$coefficients
+         
+       } else {
+         out <- predict(x, s = s)$coefficients
+         
+       }
+       names(out)[out != 0]
+     },
+     tags = c("Linear Regression", "L2 Regularization"),
+     prob = NULL,
+     sort = function(x) x[order(-x$lambda),])
 
 
-#RidgeModel$ridgefit()
+
+
 
 Ridge_fitting <- train(rm ~ ., data = training, 
                    method = RidgeModel, 
@@ -94,4 +119,3 @@ Ridge_fitting <- train(rm ~ ., data = training,
                    trControl = fitControl)
 Ridge_fitting
 # ridgereg_own_fit <- caret::train(rm ~ ., data = training, method = "RidgeModel")
-# print(ridgere)
