6ad4f925f789f3cd40228786bb06646dcdda896c
rabnawaz jansher <rabnawazjansher8@gmail.com>
2017-10-14 02:14:43 +0200

    finalize code

--- vignettes/ridgereg.R
+++ vignettes/ridgereg.R
@@ -20,43 +20,41 @@ ridgereg_forward_fit <- train(rm ~ ., data = training, method = "leapForward")
 print(ridgereg_forward_fit)
 
 ## ------------------------------------------------------------------------
-ridge_custom <- list(type=c("Regression"),library="statPack",loop=NULL,prob=NULL)
+ridgemodel <- list(type= "Regression" ,library="statPack",loop=NULL,prob=NULL)
 
-# ridge_custom$parameters <- data.frame(parameter="lambda",class="numeric",label="lambda")
-# 
-# ridge_custom$grid <- function(y,x, len=NULL, search="grid")
-# {
-#     if(search == "grid") {
-#     out <- expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)))
-#   } else {
-#     out <- data.frame(lambda = 10^runif(len, min = -5, 1))
-#   }
-#   out
-# 
-# 
-# }
-# 
-# ridge_custom$fit <- function (x, y, wts, param, lev, last, classProbs, ...)
-# {
-# if (is.data.frame(x))
-#     dat<- x
-#   else as.data.frame(x)
-#     dat$.outcome <- y
-#   result <- ridgereg$new(dat$.outcome ~ ., data = dat, lambda=param$lambda, ...)
-#   result
-# }
-# 
-# ridge_custom$predict <- function (modelFit, newdata, submodels = NULL) {
-#   if (!is.data.frame(newdata))
-#   {
-#     newdata <- as.data.frame(newdata)
-#   }
-#   modelFit$predict(newdata)
-# }
-# 
-# 
-# set.seed(12345)
-# result <- train( rm ~ ., data=training, method="ridge")
-# print(result)
+ridgemodel$parameters <- data.frame(parameter="lambda",class="numeric",label= "Ridge Regression")
 
+ridgemodel$grid <- function(y,x, len=NULL, search="grid")
+{
+  if(search == "grid") {
+    out <- expand.grid(lambda = c(0, 10 ^ seq(-1, -4, length = len - 1)))
+  } else {
+    out <- data.frame(lambda = 10^runif(len, min = -5, 1))
+  }
+  out
+
+
+}
+
+ridgemodel$fit <- function (x, y, wts, param, lev, last, classProbs, ...)
+{
+  dat <- if (is.data.frame(x))
+     x
+  else as.data.frame(x)
+  dat$.outcome <- y
+  result <- statPack::ridgereg$new(dat$.outcome ~ ., data = dat, lambda=param$lambda, ...)
+  result
+}
+
+ridgemodel$predict <- function (modelFit, newdata, submodels = NULL) {
+  if (!is.data.frame(newdata))
+  {
+    newdata <- as.data.frame(newdata)
+  }
+  modelFit$predict(newdata)
+}
+
+## ---- echo=TRUE , results= "hide", warning=FALSE , message=FALSE---------
+#result <- train( rm ~ ., data=training, method= ridgemodel, trControl = fitControl)
+#result
 
